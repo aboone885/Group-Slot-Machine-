217b26de49eeec78bb2a2eb853411e8205fdb83d
Enrico De Flaviis <23deflaviienrico@gmail.com>
2025-07-09 22:18:23 -0700

    change win message

--- slot_machine/slotmachine/src/components/WinMessage.js
+++ slot_machine/slotmachine/src/components/WinMessage.js
@@ -5,7 +5,7 @@ function WinMessage() {
   return (
     <div className="win-message">
       <h3>🎉 Congratulations! 🎉</h3>
-      <p>You won! Three matching emojis!</p>
+      <p>You won!</p>
       <div className="celebration">🎊🎊🎊</div>
     </div>
   );
